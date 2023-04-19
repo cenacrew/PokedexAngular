1ad4ad98aae52020b916c18737e315595193002f
cenacrew <valetnina.sp@gmail.com>
2023-04-19 16:38:02 +0200

    adding a pokemonDetails component

---
+++ src/app/modules/pokemons/pokemon-details/pokemon-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-details',
+  templateUrl: './pokemon-details.component.html',
+  styleUrls: ['./pokemon-details.component.scss']
+})
+export class PokemonDetailsComponent {
+
+}
--- src/app/modules/pokemons/pokemons.module.ts
+++ src/app/modules/pokemons/pokemons.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { PokemonsListComponent } from './pokemons-list/pokemons-list.component';
 import { SharedModule } from '@/shared/shared.module';
 import { PokemonsListItemComponent } from './pokemons-list-item/pokemons-list-item.component';
+import { PokemonDetailsComponent } from './pokemon-details/pokemon-details.component';
 
 
 @NgModule({
   declarations: [
     PokemonsListComponent,
     PokemonsListItemComponent,
+    PokemonDetailsComponent,
   ],
   imports: [
     CommonModule,
