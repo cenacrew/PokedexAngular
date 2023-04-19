d1abc725354bb6bcb7cf3f8c0f9103a398e75831
cenacrew <valetnina.sp@gmail.com>
2023-04-19 09:48:23 +0200

    remove unused import

--- src/app/modules/pokemons/pokemons-list/pokemons-list.component.ts
+++ src/app/modules/pokemons/pokemons-list/pokemons-list.component.ts
@@ -1,4 +1,4 @@
-import { Component,Input} from '@angular/core';
+import { Component } from '@angular/core';
 
 import { Pokemon } from '@/models';
 import { PokemonService } from 'src/app/services/pokemon.service';
