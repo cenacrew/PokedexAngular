3d85444a2aa1733a2de84a41f6cd45c7b6a00aab
cenacrew <valetnina.sp@gmail.com>
2023-04-21 16:30:30 +0200

    0 power for no damage attack

--- src/app/services/move.service.ts
+++ src/app/services/move.service.ts
@@ -17,10 +17,10 @@
         map((response: any) => {
             const id = response.id;
             const name = response.name;
-            const accuracy = response.accuracy;
+            const accuracy = response.accuracy ? response.accuracy : 100;
             const effect_chance = response.effect_chance;
             const pp = response.pp;
-            const power = response.power;
+            const power = response.power ? response.power : 0;
             const damage_class = response.damage_class.name;
             const effect_entries = response.effect_entries[0] ? response.effect_entries[0].short_effect.replace(/\$effect_chance/g, effect_chance) : "N/A";            const type = response.type.name;
             return { id, name, accuracy, effect_chance, pp, power, damage_class, effect_entries, type  } as Move;
