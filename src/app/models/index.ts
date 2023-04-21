5b8a8f2946f1926208fefe274deafeb05b26776a
cenacrew <valetnina.sp@gmail.com>
2023-04-21 11:35:42 +0200

    adding a move model

--- src/app/models/index.ts
+++ src/app/models/index.ts
@@ -1,1 +1,2 @@
 export * from './pokemon.model';
+export * from './move.model';
---
+++ src/app/models/move.model.ts
@@ -0,0 +1,11 @@
+export type Move = {
+    id:number;
+    name:string;
+    accuracy:number;
+    effect_chance:number;
+    pp:number;
+    power:number;
+    damage_class:string;
+    effect_entries:string;
+    type:string;
+}
